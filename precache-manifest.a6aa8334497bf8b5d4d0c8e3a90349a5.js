f2c4a493ea3df7a318e19f41239be9adc0e8dae0
PatrickRamsayGilbert <i_be_pretty@yahoo.com>
2020-12-27 14:09:16 -0500

    Updates

---
+++ precache-manifest.a6aa8334497bf8b5d4d0c8e3a90349a5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6f674a6c684aaf545d056ee9ba06bee5",
+    "url": "/instructor-change/index.html"
+  },
+  {
+    "revision": "c789b0cfcadbfc0d409e",
+    "url": "/instructor-change/static/css/main.9ae428d3.chunk.css"
+  },
+  {
+    "revision": "8c0e1869fd65932e04af",
+    "url": "/instructor-change/static/js/2.bb72e0cb.chunk.js"
+  },
+  {
+    "revision": "c789b0cfcadbfc0d409e",
+    "url": "/instructor-change/static/js/main.c101c399.chunk.js"
+  },
+  {
+    "revision": "76bf08ee11b3c67301ac",
+    "url": "/instructor-change/static/js/runtime-main.5842d7ba.js"
+  }
+]);
